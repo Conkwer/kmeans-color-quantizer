f06b145e9b905ea98230e26a5172d49fd907b29c
Conkwer <oketado@gmail.com>
2025-12-25 04:05:32 +0300

    Add transparency support and alpha mask export
    - Add --trans-color option to replace transparency with custom color
    - Add --save-alpha to export alpha channel as separate grayscale mask
    - Support for retro game sprites (BennuGD, Genesis/Mega Drive)
    - Auto-expand palette to 256 colors when using transparency
    - Update README with new features and examples

--- convert.cmd
+++ convert.cmd
@@ -1,4 +1,4 @@
-kmeans_8bit.exe example.png -o example_kmeans.png
+python kmeans_8bit.py alpha.png --save-alpha -f BMP
 
 ::python kmeans_8bit.py photo.png -f BMP -o photo_kmeans.bmp
 ::python kmeans_8bit.py photo.png -d -f BMP -o photo_8bit_dithering.bmp
